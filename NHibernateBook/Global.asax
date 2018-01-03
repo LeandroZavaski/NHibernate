57ef1f3bcf440e4a13de006dec0598a418b2ec4f
leandro <leandro@IDEA-PC>
2018-01-03 14:44:38 -0200

    Add project files.

---
+++ NHibernateBook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NHibernateBook.MvcApplication" Language="C#" %>
---
+++ NHibernateBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NHibernateBook.Startup))]
+namespace NHibernateBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
